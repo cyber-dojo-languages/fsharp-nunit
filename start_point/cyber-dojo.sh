46a4a39cb693882f5e3ecc176619685b44e47d0f
Jon Jagger <jon@jaggersoft.com>
2017-07-13 10:01:31 +0100

    use [apt-get install nunit-console] to install nunit

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,12 +1,8 @@
-NUNIT_PATH=/nunit/NUnit.2.6.2/lib
-export MONO_PATH=${NUNIT_PATH}
-
 fsharpc --nologo \
         --target:library \
-        -r:${NUNIT_PATH}/nunit.framework.dll \
+        -r:/usr/lib/cli/nunit.framework-2.6.3/nunit.framework.dll \
           -o RunTests.dll *.fs
 
 if [ $? -eq 0 ]; then
-  NUNIT_RUNNERS_PATH=/nunit/NUnit.Runners.2.6.1/tools
-  mono ${NUNIT_RUNNERS_PATH}/nunit-console.exe -nologo ./RunTests.dll
+  nunit-console -nologo ./RunTests.dll
 fi
